5fc06ac865b64c707dc7202e3b518ae56cd1d939
Colin Jacobs <colin@coljac.net>
2024-04-27 14:41:32 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # nextcloud-status
 A command line utility to set your nextcloud status and status message.
+
+## Installation
+
+`pip install nextcloud-status`
+
+or 
+
+`pip install https://github.com/coljac/nextcloud-status`
+
+for the latest version if it's not on PyPi.
+
+## Usage
+
+The first time you run, it will prompt you for your NextCloud url, username and password. I recommend making an app-specific password: [See](https://help.nextcloud.com/t/where-to-create-app-password/157454). Config will be in `~/.config/nextcloud-status` on unix or in `AppData/Local` on Windows.
+
+For help:
+
+![docs/help.gif]
+
+Or for sub-commands:
+
+![docs/help-sub.gif]
+
+To set the status and message:
+
+![docs/set.gif]
+
+To use an emoji:
+
+![docs/set-emoji.gif]
+
+To use github markup:
+
+![docs/set-gh.gif]
+
+Please create an issue with any bugs.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nextcloud-status',
+    version='0.1.0',
+    description='A CLI tool for updating NextCloud status',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Colin Jacobs',
+    author_email='colin@coljac.net',
+    url='https://github.com/coljac/nextcloud-status',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'typer'
+    ],
+    entry_points={
+        'console_scripts': [
+            'nextcloud-status=nextcloud_status.nextcloud_status:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
